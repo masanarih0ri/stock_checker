055a232f1209fa0d37a235de9573a62c46eff234
masanarih0ri <masanarihoridevelop@gmail.com>
2020-12-06 22:43:47 +0900

    エラーページの作成

---
+++ layouts/error.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h1 v-if="error.statusCode === 404">指定されたページは見つかりませんでした。</h1>
+    <h1 v-else>エラーが発生しました。</h1>
+    <nuxt-link to="/">トップページへ戻る</nuxt-link>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    error: { type: Object, default () { return {} } }
+  }
+}
+</script>
