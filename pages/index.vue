22beeee89b86ca522faa002d3b31559c930d7379
masanarih0ri <masanarihoridevelop@gmail.com>
2020-12-06 23:43:28 +0900

    indexページを追加

---
+++ pages/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <h1 class="red">
+    Hello!
+  </h1>
+</template>
+
+<script>
+export default {
+  // page properties go here
+}
+</script>
+
+<style>
+  .red {
+    color: red;
+  }
+</style>
